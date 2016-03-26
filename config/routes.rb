3d09b312d20811a25749c560e7854306258e812d
eramon224 <eramon224@tamu.edu>
2016-03-26 17:02:57 +0000

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,8 @@ Rails.application.routes.draw do
   get 'advisor/info'
 
   get 'sessions/new'
+  
+  get 'sessions/login'
 
   resources :advisor_users
   get 'advisor/index'
@@ -76,8 +78,6 @@ Rails.application.routes.draw do
 
   get 'registration_home/index'
   
-  
-
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
