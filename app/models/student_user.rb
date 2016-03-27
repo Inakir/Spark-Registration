1d118fafa560781880455e320f66bedf49132f83
izayahallen <izayahallen@gmail.com>
2016-03-27 17:57:57 +0000

    Messing around w/ required filds in student_user forms

--- app/models/student_user.rb
+++ app/models/student_user.rb
@@ -9,9 +9,9 @@ class StudentUser < ActiveRecord::Base
   end
 end
         
-  before_save { self.username = username.downcase }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :username, presence: true, length: {maximum: 255}, 
+  validates :email, presence: true, length: {maximum: 255}, 
   			format: { with: VALID_EMAIL_REGEX }, 
 			uniqueness: { case_sensitive: false }
 
@@ -20,10 +20,9 @@ end
   validates :school_name, presence: true, length: {maximum: 60}
   validates :school_level, presence: true, #length: {maximum: 60}, 
   			inclusion: { in: %w(Elementary Middle High), message: "%{value} is not a valid school level must be Elementary, Middle, or High " }
-  validates :pay_code, presence: true
   has_secure_password
-  validates :password, length: { minimum: 6 }, confirmation: true
-  validates_confirmation_of :password
+ # validates :password, length: { minimum: 6 }, confirmation: true
+  #validates_confirmation_of :password
 
   
 end
