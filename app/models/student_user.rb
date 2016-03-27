ec0ee996008ac2222562b689b5bb592c80c9a6bb
izayahallen <izayahallen@gmail.com>
2016-03-27 18:03:26 +0000

    test

--- app/models/student_user.rb
+++ app/models/student_user.rb
@@ -18,11 +18,12 @@ end
   validates :first_name, presence: true, length: {maximum: 60}
   validates :last_name, presence: true, length: {maximum: 60}
   validates :school_name, presence: true, length: {maximum: 60}
-  validates :school_level, presence: true, #length: {maximum: 60}, 
-  			inclusion: { in: %w(Elementary Middle High), message: "%{value} is not a valid school level must be Elementary, Middle, or High " }
+  #validates :school_level, presence: true, #length: {maximum: 60}, 
+  			#inclusion: { in: %w(Elementary Middle High), message: "%{value} is not a valid school level must be Elementary, Middle, or High " }
+  #validates :pay_code, presence: true
   has_secure_password
- # validates :password, length: { minimum: 6 }, confirmation: true
-  #validates_confirmation_of :password
+  validates :password, length: { minimum: 6 }, confirmation: true
+  validates_confirmation_of :password
 
   
 end
