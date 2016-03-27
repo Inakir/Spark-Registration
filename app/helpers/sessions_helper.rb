9d480991ab7dc7199fffa977b12f23df6e6b919a
teahouse11 <austinktang@gmail.com>
2016-03-27 20:26:14 +0000

    added methods to session helper for student user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,20 +9,12 @@ module SessionsHelper
   end
 
   def logged_in?
-    !current_user.nil? && current_user.usertype=="advisor"
+    !current_user.nil? && (current_user.usertype=="advisor" || current_user.usertype=="student")
   end
   
-  def student_log_in(user)
-    session[:user_id] = user.id
-  end
-
   # find current user in this session
   def student_current_user
-    @admin_current_user ||= StudentUser.find_by(id: session[:user_id])
+    @current_user ||= StudentUser.find_by(id: session[:user_id])
   end
 
-  def student_logged_in?
-    !student_current_user.nil? && student_current_user.usertype=="student"
-  end
-  
 end
