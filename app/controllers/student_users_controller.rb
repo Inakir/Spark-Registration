d5f02bfdf5b80ef6e2998ec618b91de1c751481d
izayahallen <izayahallen@gmail.com>
2016-04-08 19:18:24 +0000

    asd

--- app/controllers/student_users_controller.rb
+++ app/controllers/student_users_controller.rb
@@ -61,7 +61,7 @@ class StudentUsersController < ApplicationController
         format.html { redirect_to @student_user, notice: 'Student user was successfully updated.' }
         format.json { render :show, status: :ok, location: @student_user }
       else
-        format.html { render :edit }
+        format.html { render '/' }
         format.json { render json: @student_user.errors, status: :unprocessable_entity }
       end
     end
