e4f2ef22968e7474234b125c32fdc4fad588ef3d
cjf2222 <cjf2222@tamu.edu>
2016-04-09 15:52:11 +0000

    new email

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
 
   post 'admins/mark_paid' => 'admins#mark_paid'
   post 'admins/send_email' => 'admins#send_email'
+  post 'admins/unpaid_email_group' => 'admins#unpaid_email_group'
 
   resources :admins do
 	member do
