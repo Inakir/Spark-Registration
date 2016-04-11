f7692af913bdf7c8bcde25ec8bfa33a3f0fccb3e
teahouse11 <austinktang@gmail.com>
2016-04-11 01:03:15 +0000

    form in html

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,8 @@ Rails.application.routes.draw do
   get 'admin_login' => 'admins#new'
   post 'admin_login' => 'admins#create'
   delete 'admin_logout' => 'admins#destroy'
-  get 'admin/email_page'
+  get 'admins/email_page'
+  
   get 'advisor/create_team' => 'advisor#create_team'
   get 'advisor/see_team'
   
