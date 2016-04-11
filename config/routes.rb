480f0f5d7e8154dad9550b4593b57ac95c0e955f
eramon224 <eramon224@tamu.edu>
2016-04-11 00:35:53 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,6 @@ Rails.application.routes.draw do
   post 'admins/mark_paid' => 'admins#mark_paid'
   post 'admins/send_email' => 'admins#send_email'
   post 'admins/unpaid_email_group' => 'admins#unpaid_email_group'
-<<<<<<< HEAD
 
   resources :admins do
 	member do
@@ -41,7 +40,7 @@ Rails.application.routes.draw do
 	  #get 'sessions/admin_new'
 	end
   end
-=======
+
   post 'admins/email_advisors' => 'admins#email_advisors'
   post 'admins/email_all' => 'admins#email_all'
   resources :admins do
