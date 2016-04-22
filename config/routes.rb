c99fb819421fc3e6abc32dc1a097aa2fec0d86c1
cjf2222 <cjf2222@tamu.edu>
2016-04-22 18:27:46 +0000

    admin info page

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   post 'admins/email_paid' => 'admins#email_paid'
   post 'admins/send_all' => 'admins#send_all'
   post 'admins/send_stud_email' => 'admins#send_stud_email'
+   post 'admins/admin_email' => 'admins#admin_email'
   post 'admins/edit_email' => 'admins#edit_email'
   post 'admins/mark_paid' => 'admins#mark_paid'
   post 'admins/send_email' => 'admins#send_email'
