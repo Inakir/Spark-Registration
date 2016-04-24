f3492f25686c205be67432f9287babd4acb15e6d
cjf2222 <cjf2222@tamu.edu>
2016-04-24 17:26:42 +0000

    changed route name

--- app/controllers/student_users_controller.rb
+++ app/controllers/student_users_controller.rb
@@ -124,15 +124,6 @@ class StudentUsersController < ApplicationController
       params.require(:student_user).permit(:first_name, :last_name, :school_level, :password, :pay_status, :password_confirmation, :school_name, :team_name, :pay_code, :team_code, :email)
     end
 	
-<<<<<<< HEAD
-# 	def sdgsdgsgsd(school_level)
-# 		logger.info("Hello World")
-# 	end
-	
-# 	def roomba2(variable)
-# 		flash.now[:flash] = "Hello World!"
-# 	end
-=======
 #	def sdgsdgsgsd(school_level)
 #		logger.info("Hello World")
 #	end
@@ -140,5 +131,4 @@ class StudentUsersController < ApplicationController
 #	def roomba2(variable)
 #		flash.now[:flash] = "Hello World!"
 #	end
->>>>>>> 480f663f65ec7eed5a6ca1680f20806f577f24bb
 end
